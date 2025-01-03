99be895a72bb0dea156b6dd5e5ee58553592dac3
Balasubramanian Shanmugham <bala.s0027@gmail.com>
2025-01-03 19:20:10 +0530

    168. Excel Sheet Column Title

---
+++ src/ExcelSheetColumnTitle.java
@@ -0,0 +1,39 @@
+/*
+ * excel-sheet-column-title - LeetCode
+ * https://leetcode.com/problems/excel-sheet-column-title/
+ * Problem statement: 
+ * ------------------
+    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+    For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+ */
+public class ExcelSheetColumnTitle {
+    public static void main(String[] args) {
+        System.out.println(convertToTitle(1)); 
+        System.out.println(convertToTitle(2));
+        System.out.println(convertToTitle(3));
+        System.out.println(convertToTitle(26));
+        System.out.println(convertToTitle(27));
+        System.out.println(convertToTitle(28));
+    }
+
+    public static String convertToTitle(int n) {
+        StringBuilder columnTitle = new StringBuilder();
+        while (n > 0) {
+            n--;
+            columnTitle.append((char) ('A' + (n % 26)));
+            n /= 26;
+        }
+
+        return columnTitle.reverse().toString();
+    }
+}
