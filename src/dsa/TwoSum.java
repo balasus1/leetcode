2160bdc292ced85c195628a2f1a44b21c080ee2c
DESKTOP-JL24J15\ADMIN <bala.s0027@gmail.com>
2025-11-10 21:46:28 +0530

    zero sum triplet

--- src/dsa/TwoSum.java
+++ src/dsa/TwoSum.java
@@ -4,13 +4,16 @@ import java.util.*;
 import java.util.Arrays;
 
 public class TwoSum {
-    public static void main(String[] args) {
+     static void main() {
         int[] twoSumProblem = getTwoSum(new int[]{2,7,11,8,1,15}, 9);
         System.out.println("TwoSum: " + Arrays.toString(twoSumProblem));
         System.out.println("TwoSum unique set: " + getTwoSumUniquePairs(new int[]{2,7,11,8,1,5,4,15}, 9));
         int[] sortedArrays = new int[]{2,3,5,7,8,9,10,14,24,38};
         int[] result = getTwoSumOnSortedArray(sortedArrays, 7);
         System.out.println("TwoSum 2: " + Arrays.toString(result));
+        int[] zeroSumTestCaseArray = {1,2,2,2,2,-2,-2,-2,-2,-1,1};
+        System.out.println("Zero Sum triplets: " + getZeroSumTriplets(zeroSumTestCaseArray));
+
     }
 
     // simple scenario
@@ -61,5 +64,35 @@ public class TwoSum {
             }
         }
         return  result;
+    }
+
+    static List<List<Integer>> getZeroSumTriplets(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+        for(int i=0; i<nums.length-1; i++){
+            // skip duplicate elements in the nums array
+            if((i > 0) && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int leftIndex=i+1, rightIndex = nums.length-1;
+            while(leftIndex < rightIndex){
+                int sum = nums[i] + nums[leftIndex] + nums[rightIndex];
+
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[leftIndex], nums[rightIndex]));
+                    // skip already visited triplets
+                    while (leftIndex < rightIndex && nums[leftIndex] == nums[leftIndex+1]) { leftIndex++; }
+                    while (leftIndex < rightIndex && nums[rightIndex] == nums[rightIndex-1]) { rightIndex--; }
+                    leftIndex++; rightIndex--;
+                } else if(sum < 0) {
+                    leftIndex++;
+                } else {
+                    rightIndex--;
+                }
+            }
+        }
+        return result;
     }
 }
